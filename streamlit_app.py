fe1a4c4d7de1b7f4cf6a4eb6b1807b23e131aa26
esgtrial <122380694+esgtrial@users.noreply.github.com>
2023-09-14 14:52:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ st.title("Generative AI using Snowflake External Functions and OpenAI")
 st.write(st.secrets.snowflake.user)
 
 # Get the current credentials
-# session = get_active_session()
+session = get_active_session()
 model = "gpt4"
