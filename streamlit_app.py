ccac6b5e5d079396acbee8c72c2074fe320dcdaf
esgtrial <122380694+esgtrial@users.noreply.github.com>
2023-09-14 14:59:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
 # Import python packages
 import streamlit as st
 import pandas as pd
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark import Session
 
 st.set_page_config(
-    page_title="Generative AI using Snowflake External Functions and OpenAI",
+    page_title="Generative AI using ❄️ Snowflake External Functions and OpenAI",
     layout='wide'
 )
 
 st.title("Generative AI using Snowflake External Functions and OpenAI")
 st.write(st.secrets.snowflake.user)
 
-# Get the current credentials
-session = get_active_session()
-model = "gpt4"
+# Establish Snowflake session
+@st.cache_resource
+def create_session():
+    return Session.builder.configs(st.secrets.snowflake).create()
+
+session = create_session()
+st.success("Connected to Snowflake!")
