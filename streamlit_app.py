6655951c3903b06bc1e5885ab4fed7562ad21d21
esgtrial <122380694+esgtrial@users.noreply.github.com>
2023-09-14 13:39:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,3 @@
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import sum, col
-import altair as alt
 import streamlit as st
 
-# Set page config
-st.set_page_config(layout="wide")
-
-# Get current session
-session = get_active_session()
+st.write(st.secrets["user"])
