9c8ed134865fb33c280e0d2bf8faa3ac5e297421
esgtrial <122380694+esgtrial@users.noreply.github.com>
2023-09-14 13:43:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.write(st.secrets.snowflake.user)
+
+
+st.set_page_config(
+    page_title="Generative AI using Snowflake External Functions and OpenAI",
+    layout='wide'
+)
+
+st.title("Generative AI using Snowflake External Functions and OpenAI")
+
